ab84f7a8bc185cc5fd2297252fdd93899708adcb
Bruno Martins <bgcngm@gmail.com>
2023-09-08 18:25:31 +0100

    wlan: cnss2: Disable spammy logging

--- qcom/wlan/platform/cnss2/pci.c
+++ qcom/wlan/platform/cnss2/pci.c
@@ -2067,8 +2067,10 @@ static int cnss_pci_set_mhi_state(struct cnss_pci_data *pci_priv,
 	if (ret)
 		goto out;
 
+	#ifndef OPLUS_BUG_STABILITY
 	cnss_pr_vdbg("Setting MHI state: %s(%d)\n",
 		     cnss_mhi_state_to_str(mhi_state), mhi_state);
+	#endif /* OPLUS_BUG_STABILITY */
 
 	switch (mhi_state) {
 	case CNSS_MHI_INIT:
@@ -4088,7 +4090,9 @@ static int cnss_pci_runtime_suspend(struct device *dev)
 		}
 	}
 
+	#ifndef OPLUS_BUG_STABILITY
 	cnss_pr_vdbg("Runtime suspend start\n");
+	#endif /* OPLUS_BUG_STABILITY */
 
 	driver_ops = pci_priv->driver_ops;
 	if (driver_ops && driver_ops->runtime_ops &&
@@ -4123,7 +4127,9 @@ static int cnss_pci_runtime_resume(struct device *dev)
 		return -EAGAIN;
 	}
 
+	#ifndef OPLUS_BUG_STABILITY
 	cnss_pr_vdbg("Runtime resume start\n");
+	#endif /* OPLUS_BUG_STABILITY */
 
 	driver_ops = pci_priv->driver_ops;
 	if (driver_ops && driver_ops->runtime_ops &&
